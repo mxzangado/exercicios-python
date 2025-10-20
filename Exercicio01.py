e2ca342ed2a17796f9b6565db9b953331261b2d9
Edson Carlos <mxzangado@gmail.com>
2025-10-20 20:16:14 -0300

    Exercícios 01

---
+++ Exercicio01.py
@@ -0,0 +1,9 @@
+# Caucúlando a Média
+print('Cálculo da Média entre duas notas')
+n1 = float(input('Digite o valor da nota: ').replace(',','.'))
+n2 = float(input('Digite o valor da nota: ').replace(',','.'))
+
+# Soma entre parenteses é dividido por 2
+media = (n1 + n2) /2
+# Resultado
+print(f'A média é {media:.2f} Parabéns!') 
