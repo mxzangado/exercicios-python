21303cf7a2a46cf8b51cd11e8b1b5ce59ec82eb5
Edson Carlos <mxzangado@gmail.com>
2025-10-20 20:41:44 -0300

    Comparando valores Com If else

---
+++ Exercicio02.py
@@ -0,0 +1,8 @@
+# Comparando dois valores A e B COM IF ELSE
+
+a = int(input('Digite o Valor de : "A" ').strip())
+b = int(input('Digite o Valor de : "B" ').strip())
+if a < b:
+    print('A é menor que B')
+else:
+    print('A não é menor que B')
