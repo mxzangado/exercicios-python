dc7fa6a95fd636beb4137442dbe64527c7f9385c
Edson <mxzangado@gmail.com>
2025-10-24 18:23:29 -0300

    Tipos de print

---
+++ Exercicio04.py
@@ -0,0 +1,13 @@
+# Tipos de prints com Python
+
+# Primeiro print
+print('Hello, Word!')
+# Print com variável nome
+nome = 'Helo, Word!'
+print(nome)
+# Print str + variável nome
+print('Quebrando a maldição, ' + nome)
+# Print com f'string
+print(f'Quebrando a maldição {nome}')
+# Print com .format
+print('Quebrei a maldição {}'.format(nome))
